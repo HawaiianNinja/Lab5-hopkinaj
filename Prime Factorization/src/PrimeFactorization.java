6fe9b220e4fd18ed0a0d0d43c5aa2cfe4d208350
Andrew Hopkins <hopkinaj@rose-hulman.edu>
2011-03-28 22:28:24 -0400

    Passed test case for 1

--- Prime Factorization/src/PrimeFactorization.java
+++ Prime Factorization/src/PrimeFactorization.java
@@ -1,4 +1,10 @@
+import java.util.ArrayList;
+
 
 public class PrimeFactorization {
+
+	public static ArrayList<Integer> factors(int i) {
+		return new ArrayList<Integer>();
+	}
 
 }
--- Prime Factorization/src/PrimeFactorizationTest.java
+++ Prime Factorization/src/PrimeFactorizationTest.java
@@ -1,4 +1,7 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
 
 import org.junit.Test;
 
@@ -6,5 +9,10 @@ public class PrimeFactorizationTest {
 	@Test
 	public void TestJunit() {
 		assertTrue("this should pass", true);
+	}
+	
+	@Test
+	public void TestOne(){
+		assertEquals(new ArrayList<Integer>(), PrimeFactorization.factors(1));
 	}
 }
