5fc4931a4ca608a360e1939706675a9816c19244
Andrew Hopkins <hopkinaj@rose-hulman.edu>
2011-03-28 22:17:25 -0400

    Adding files and passed first test

---
+++ Prime Factorization/src/PrimeFactorizationTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PrimeFactorizationTest {
+	@Test
+	public void TestJunit() {
+		assertTrue("this should pass", true);
+	}
+}
